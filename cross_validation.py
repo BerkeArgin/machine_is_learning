641a6f31ae418dd4e7c71ec38189b53d1fefef33
Zeynep Tandogan <zeyneptandogann4@gmail.com>
2023-10-30 01:11:39 +0100

    organization and readme added

---
+++ cross_validation.py
@@ -0,0 +1,146 @@
+import numpy as np
+from preprocess import *
+from itertools import product
+
+def stratified_K_fold(y,k=5,seed=42):
+    np.random.seed(seed)
+    no_of_test_labels=np.floor(y.shape[0]/k)
+    
+    unique_labels=np.unique(y)
+    label_ratio={};label_indexes={};label_count_p_fold={}
+
+    picked_count=0
+    for i,label in enumerate(unique_labels):
+        label_ratio[label]=y[y==label].shape[0]/y.shape[0]
+        label_indexes[label]=np.random.permutation(np.argwhere(y==label))
+        if i<len(unique_labels)-1:
+            label_count_p_fold[label]=np.floor(no_of_test_labels*label_ratio[label])
+            picked_count+=label_count_p_fold[label]
+        else:
+            label_count_p_fold[label]=no_of_test_labels-picked_count
+
+    folds=[]
+    for i in range(k):
+        index_arrs=[]
+        for label in unique_labels:
+            label_to_take=int(label_count_p_fold[label])
+            index_arrs.append(label_indexes[label][i*label_to_take:(i+1)*label_to_take].flatten())
+    
+        test_fold=np.random.permutation(np.concatenate(index_arrs))
+        train_fold=np.random.permutation(np.setdiff1d(np.indices(y.shape),test_fold,assume_unique=True))
+        folds.append((train_fold,test_fold))
+    
+    return folds
+
+def calculate_metrics(y_pred,y_true):
+    accuracy=y_pred[y_pred==y_true].shape[0]/y_pred.shape[0]
+    precision={};recall={}
+    for label in np.unique(y_true):
+        try:
+            precision[label]=y_pred[(y_pred==label) & (y_true==label)].shape[0]/y_pred[y_pred==label].shape[0]
+        except:
+            continue
+        try:
+            recall[label]=y_pred[(y_pred==label) & (y_true==label)].shape[0]/y_true[y_true==label].shape[0]
+        except:
+            continue
+    
+    f1_scores = {}
+    for label in precision.keys():
+        if precision[label] + recall[label] != 0:
+            f1_scores[label] = 2 * (precision[label] * recall[label]) / (precision[label] + recall[label])
+        else:
+            f1_scores[label] = 0.0
+
+    return accuracy,precision,recall,f1_scores
+
+def calculate_confusion_mat(y_pred,y_true):
+    unique_labels=np.unique(y_true)
+    conf_matrix=np.zeros((len(unique_labels),len(unique_labels)))
+    #print(conf_matrix.shape)
+    for i,pred_label in enumerate(unique_labels):
+        for j,true_label in enumerate(unique_labels):
+            conf_matrix[i,j]=y_pred[(y_pred==pred_label) & (y_true==true_label)].shape[0]
+    return conf_matrix
+
+def cross_validate(X_train,y_train,k_folds,train_func,loss_function,predict_function,*args,**kwargs):
+    results=[]
+    for i,(train_fold,val_fold) in enumerate(k_folds):
+        results_dict={}
+        #print("Fold",i+1)
+        x_train_fold, x_val_fold = (X_train[train_fold],X_train[val_fold])
+        y_train_fold=y_train[train_fold]
+        y_val_fold=y_train[val_fold]
+        
+        x_train_fold,mean,std=standardize(x_train_fold)
+        x_val_fold,_,_=standardize(x_val_fold,mean,std)
+
+        
+        initial_w=np.zeros((x_train_fold.shape[1],))
+
+        X_undersampled,y_undersampled=undersample(X_train,y_train)
+
+        try:
+            w,loss=train_func(y_undersampled,X_undersampled,initial_w=initial_w,*args,**kwargs)
+        except:
+            w,loss=train_func(y_undersampled,X_undersampled,*args,**kwargs)
+        
+        y_pred,scores = predict_function(X_undersampled,w,threshold=0.5)
+        tr_accuracy,tr_precision,tr_recall,tr_f1_scores=calculate_metrics(y_undersampled,y_pred)
+        
+        y_pred,scores = predict_function(x_val_fold,w,threshold=0.5)
+        vl_accuracy,vl_precision,vl_recall,vl_f1_scores=calculate_metrics(y_val_fold,y_pred)
+
+
+        results_dict["name"]=train_func.__name__
+        results_dict["loss_type"]=loss_function.__name__
+        results_dict["predict_type"]=predict_function.__name__
+
+        results_dict.update(kwargs)
+        results_dict["w"]=w
+        results_dict["fold"]=i+1
+        
+        results_dict["tr_loss"]=loss
+        results_dict["tr_accuracy"]=tr_accuracy
+        results_dict["tr_precision"]=tr_precision
+        results_dict["tr_recall"]=tr_recall
+        results_dict["tr_f1_scores"]=tr_f1_scores
+
+        results_dict["vl_loss"]=loss_function(y_val_fold,x_val_fold,w)
+        results_dict["vl_accuracy"]=vl_accuracy
+        results_dict["vl_precision"]=vl_precision
+        results_dict["vl_recall"]=vl_recall
+        results_dict["vl_f1_scores"]=vl_f1_scores
+        
+        
+        results_dict["conf_matrix"]=calculate_confusion_mat(y_pred,y_val_fold)
+
+        results.append(results_dict)
+    
+    return results
+
+def apply_cross_val(functions, grid_search_dict, function_kwargs, X_train,y_train,k_folds ):
+
+    all_results=[]
+    for i,function in enumerate(functions):
+        hyperparameters = function_kwargs[function[0].__name__]
+
+        print(f"{i+1}/{len(functions)} STARTING: ({function[0].__name__}, {function[1].__name__} , {function[2].__name__})")
+        
+        if len(hyperparameters)==0:
+            results=cross_validate(X_train,y_train,k_folds,*function)
+            all_results.extend(results)
+            continue
+
+        param_combinations = product(*[grid_search_dict[param] for param in hyperparameters])
+        
+        for param in param_combinations:        
+            kwarg=dict(zip(hyperparameters,list(param)))
+            results=cross_validate(X_train,y_train,k_folds,*function,**kwarg)
+            print(kwarg)
+            all_results.extend(results)
+        
+        print(f"{i+1}/{len(functions)} DONE: ({function[0].__name__}, {function[1].__name__} , {function[2].__name__})")
+        return all_results
+
+
---
+++ implementations.py
@@ -0,0 +1,370 @@
+from helpers import *
+import numpy as np
+
+
+def calculate_mse_loss(y, tx, w):
+    """
+    Calculate Mean Absolute Error Loss
+    Args:
+        y: A numpy array of shape (N,) containing the observed outputs.
+        tx: A numpy array of shape (N, D) containing the feature matrix of the data.
+        w: A numpy array of shape (D,) which is the weight vector.
+
+    Returns:
+       The Mean Squared Error Loss between observed and predicted outputs
+
+    """
+    y_pred = tx.dot(w)
+    return np.mean((y - y_pred) ** 2) / 2
+
+
+def calculate_gradient(y, tx, w):
+    """
+    This function calculates the gradient at w and return gradient and error values.
+    Args:
+        tx: A numpy array of shape (N,) containing the observed outputs.
+        error:
+
+    Returns:
+       The gradient of least squares (shape (D,)) and the error between observed and predicted outputs (shape (N,))
+    """
+    error = y - tx.dot(w)
+    return -tx.T.dot(error) / error.size
+
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Args:
+        y:  The output desired values.
+        tx: The input data.
+        batch_size: Size of each mini-batch.
+        num_batches: The number of mini-batches to produce.
+        shuffle: If True, the data is shuffled; otherwise, data is taken sequentially.
+
+    Returns:
+       Containing the output labels and input data for a mini-batch.
+    """
+    data_size = len(y)
+    batch_size = min(data_size, batch_size)  # Limit the possible size of the batch.
+    max_batches = int(
+        data_size / batch_size
+    )  # The maximum amount of non-overlapping batches that can be extracted from the data.
+    remainder = (
+            data_size - max_batches * batch_size
+    )
+    if shuffle:
+        idxs = np.random.randint(max_batches, size=num_batches) * batch_size
+        if remainder != 0:
+            # Add an random offset to the start of each batch to eventually consider the remainder points
+            # Without an offset, batches might start at indices: [0, 32, 64, 96] (assume remainder=4)
+            # With a random offset, batches would start at: [4, 36, 68, 100]
+            idxs += np.random.randint(remainder + 1, size=num_batches)
+    else:
+        idxs = np.array([i % max_batches for i in range(num_batches)]) * batch_size
+    for start in idxs:
+        start_index = start
+        end_index = (
+                start_index + batch_size
+        )
+        yield y[start_index:end_index], tx[start_index:end_index]
+
+def sigmoid(x):
+    """
+    Applies the sigmoid function to a given input.
+    Args:
+        x: Given input.
+    Returns:
+        The value of sigmoid function.
+    """
+    epsilon = 1e-15  # A small constant to avoid division by zero or log(0)
+
+    pred_probs = np.piecewise(
+        x,
+        [x > 0],
+        [lambda i: 1 / (1 + np.exp(-i)), lambda i: np.exp(i) / (1 + np.exp(i))],
+    )
+    return np.clip(pred_probs, epsilon, 1 - epsilon)
+
+def calculate_logistic_loss(y, tx, w):
+    """
+    Compute the negative log likelihood for logistic regression.
+
+    Args:
+        y: A numpy array of shape (N,) containing the observed outputs.
+        tx: A numpy array of shape (N, D) containing the feature matrix of the data.
+        w: A numpy array of shape (D,) which is the weight vector.
+
+    Returns:
+        Negative log likelihood loss.
+    """
+    t = np.dot(tx, w)
+    loss = np.sum(np.log(1 + np.exp(t)) - y * t) / y.shape[0]
+    return loss
+
+
+def calculate_logistic_gradient(y, tx, w):
+    """
+    Compute the gradient of the negative log likelihood for logistic regression.
+
+    Args:
+        y: A numpy array of shape (N,) containing the observed outputs.
+        tx: A numpy array of shape (N, D) containing the feature matrix of the data.
+        w: A numpy array of shape (D,) which is the weight vector.
+
+    Returns:
+        gradient_vector: Gradient vector which has shape (D,)
+    """
+    predicted_probs = sigmoid(tx.dot(w))
+    gradient_vector = tx.T.dot(predicted_probs - y)
+    return gradient_vector
+
+
+
+
+
+
+########################## FUNCTIONS #####################################################
+
+def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
+    """
+     Linear regression using gradient descent
+     Args:
+         y: Vector of labels. (shape (N,))
+        tx: Matrix of features/input data. (shape (N,D))
+        initial_w: Initial vector of weights for the model. (shape (D, ))
+        max_iters: Maximum number of iterations for the optimization.
+        gamma: Learning rate.
+     Returns:
+        weigth_list[-1]:Optimized weight vector after gradient descent
+        loss_values[-1]: The final loss value.
+     """
+    weigth_list = [initial_w]
+    w = initial_w
+    loss_values = [calculate_mse_loss(y, tx, w)]
+    for iter_num in range(max_iters):
+        # compute loss and gradient
+        gradient_vector = calculate_gradient(y, tx, w)
+        # update the weights
+        w = w - gamma * gradient_vector
+        loss = calculate_mse_loss(y, tx, w)
+        weigth_list.append(w)
+        loss_values.append(loss)
+        print("Mean Squared Error GD => {0}/{1}: loss={2}".format(iter_num, max_iters - 1,
+                                                                  loss))  # for tracking the situation
+    return weigth_list[-1], loss_values[-1]
+
+def mean_squared_error_sgd(y, tx, initial_w,max_iters, gamma):
+    """
+         Linear regression using stochastic gradient descent
+         Args:
+             y: Vector of labels. (shape (N,))
+            tx: Matrix of features/input data. (shape (N,D))
+            initial_w: Initial vector of weights for the model. (shape (D, ))
+            max_iters: Maximum number of iterations for the optimization.
+            gamma: Learning rate.
+         Returns:
+            weigth_list[-1]:Optimized weight vector after stochastic gradient descent
+            loss_values[-1]: The final loss value.
+         """
+    w = initial_w
+    weigth_list = [initial_w]
+    loss_values = [calculate_mse_loss(y, tx, w)]
+
+    for n_iter in range(max_iters):
+        for yn, txn in batch_iter(y, tx, 1, 1):
+            gradient_vector = calculate_gradient(yn, txn, w)
+            w = w - gamma * gradient_vector
+            loss = calculate_mse_loss(yn, txn, w)
+            weigth_list.append(w)
+            loss_values.append(loss)
+        print("Stochastic Gradient Descent({bi}/{ti}): loss={l}".format(
+            bi=n_iter, ti=max_iters - 1, l=loss))  # for tracking the situation
+    return weigth_list[-1], loss_values[-1]
+
+def least_sq_predict(x, w, threshold=0.5):
+    scores = x.dot(w)
+    y_pred = np.zeros_like(scores)
+    y_pred[scores > threshold] = 1
+    return y_pred, scores
+
+def least_squares(y, tx):
+    """
+    Linear regression with normal equations
+     Args:
+         y: Vector of labels. (shape (N,))
+        tx: Matrix of features/input data. (shape (N,D)).
+     Returns:
+        weigth_list[-1]: Optimized weight vector.
+        loss_values[-1]: The final loss value.
+    """
+    # Calculate the terms of the normal equations
+    X_transpose_X = tx.T.dot(tx)
+    X_transpose_y = tx.T.dot(y)
+
+    # Solve for weights w
+    w = np.linalg.solve(X_transpose_X, X_transpose_y)
+    loss = calculate_mse_loss(y, tx, w)
+    return w, loss
+
+
+
+def ridge_regression(y, tx, lambda_ ):
+    """
+    Ridge regression using normal equations
+     Args:
+         y: Vector of labels. (shape (N,))
+        tx: Matrix of features/input data. (shape (N,D)).
+        lambda_: Regularization parameter.
+     Returns:
+        weigth_list: Optimized weight vector.
+        loss_values: The final loss value.
+    """
+
+    I = np.identity(tx.shape[1])
+    X_transpose_X = tx.T.dot(tx)
+    X_transpose_y = tx.T.dot(y)
+    coef_mat = X_transpose_X + 2 * len(y) * lambda_ * I #2len(y) = 2N is for deleting 1/2N operation
+    constant_vector = X_transpose_y
+
+    # Solve for weights w
+    w = np.linalg.solve(coef_mat, constant_vector)
+    loss = calculate_mse_loss(y, tx, w)
+    return w, loss
+
+def logistic_regression(y, tx, initial_w,max_iters, gamma):
+    """
+    Logistic Regression using Gradient Descent.
+     Args:
+        y: Vector of labels. (shape (N,))
+        tx: Matrix of features/input data. (shape (N,D))
+        initial_w: Initial vector of weights for the model. (shape (D, ))
+        max_iters: Maximum number of iterations for the optimization.
+        gamma: Learning rate.
+
+     Returns:
+        weigth_list: Optimized weight vector.
+        loss_values: The final loss value.
+    """
+
+    weigth_list = [initial_w]
+    loss_values = []
+    w = initial_w
+    for _ in range(max_iters):
+        gradient_vector = calculate_logistic_gradient(y, tx, w)
+        w = w - gamma * gradient_vector
+        loss = calculate_logistic_loss(y, tx, w)
+
+        weigth_list.append(w)
+        loss_values.append(loss)
+    return weigth_list[-1], loss_values[-1]
+
+############################# Logistic Regression with Regularization ##############################
+
+def logit_predict(x,w,threshold=0.5):
+    scores=sigmoid(x.dot(w))
+    y_pred=np.zeros_like(scores)
+    y_pred[scores>threshold]=1
+    return y_pred,scores
+
+def calculate_logistic_regression_regularized(y, tx, w, lambda_, predictions):
+    """
+        Compute the gradient of the negative log likelihood for logistic regression with regularization term for l2".
+
+        Args:
+            y: A numpy array of shape (N,) containing the observed outputs.
+            tx: A numpy array of shape (N, D) containing the feature matrix of the data.
+            w: A numpy array of shape (D,) which is the weight vector.
+            lambda_: Regularization parameter.
+            predictions: The result of sigmoid function.
+
+        Returns:
+            gradient_vector_regularized: Gradient vector which has shape (D,)
+        """
+    gradient_vector_regularized = tx.T.dot(predictions - y) / y.shape[0] + 2 * lambda_ * w
+    return gradient_vector_regularized
+
+def reg_logistic_regression(y, tx, lambda_ ,initial_w, max_iters, gamma):
+    """
+        Regularized logistic regression using gradient descent
+         Args:
+            y: Vector of labels. (shape (N,))
+            tx: Matrix of features/input data. (shape (N,D))
+            initial_w: Initial vector of weights for the model. (shape (D, ))
+            max_iters: Maximum number of iterations for the optimization.
+            gamma: Learning rate.
+
+         Returns:
+            weigth_list[-1]: Optimized weight vector.
+            loss_values[-1]: The final loss value.
+        """
+    weight_list = [initial_w]
+    loss_values = []
+    w = initial_w
+
+    for _ in range(max_iters):
+        predictions = sigmoid(tx.dot(w))
+        gradient = calculate_logistic_regression_regularized(y, tx, w, lambda_, predictions)
+        w = w - gamma * gradient
+        loss = calculate_logistic_loss(y, tx, w)
+
+        weight_list.append(w)
+        loss_values.append(loss)
+
+    return weight_list[-1], loss_values[-1]
+
+############################# Ridge Regression with regularization ##############################
+
+def ridge_predict(x, w, threshold=0.5):
+    scores = x.dot(w)
+    y_pred = np.zeros_like(scores)
+    y_pred[scores > threshold] = 1
+    return y_pred, scores
+
+def calculate_ridge_regression_regularized(y, tx, w, lambda_):
+    """
+    Compute the gradient of the loss for ridge regression with L2 regularization term.
+
+    Args:
+    y: A numpy array of shape (N,) containing the observed outputs.
+    tx: A numpy array of shape (N, D) containing the feature matrix of the data.
+    w: A numpy array of shape (D,) which is the weight vector.
+    lambda_: Regularization parameter.
+
+    Returns:
+    gradient_vector_regularized: Gradient vector which has shape (D,)
+    """
+    N = len(y)
+    gradient_vector_regularized = -tx.T.dot(y - tx.dot(w)) / N + 2 * lambda_ * w
+    return gradient_vector_regularized
+
+def ridge_regression_gd(y, tx, lambda_, initial_w, max_iters, gamma):
+    """
+    Regularized ridge regression using gradient descent.
+
+    Args:
+        y: Vector of labels. (shape (N,))
+        tx: Matrix of features/input data. (shape (N,D))
+        lambda_: Regularization parameter.
+        initial_w: Initial vector of weights for the model. (shape (D, ))
+        max_iters: Maximum number of iterations for the optimization.
+        gamma: Learning rate.
+
+    Returns:
+        weigth_list[-1]: Optimized weight vector.
+        loss_values[-1]: The final loss value.
+    """
+    weight_list = [initial_w]
+    loss_values = []
+    w = initial_w
+
+    for _ in range(max_iters):
+        print(w)
+        gradient = calculate_ridge_regression_regularized(y, tx, w, lambda_)
+        w = w - gamma * gradient
+        loss = calculate_mse_loss(y, tx, w)
+
+        weight_list.append(w)
+        loss_values.append(loss)
+
+    return weight_list[-1], loss_values[-1]
